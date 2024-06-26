cb33a12b6d405ebbe41e47aaf7b968c0e97ae2bd
DoneRus <bogdan.bunea07@gmail.com>
2024-06-26 02:40:00 +0100

    Update forgot_process.php

--- php/forgot_process.php
+++ php/forgot_process.php
@@ -1,53 +1,63 @@
-<?php 
- require_once("config.php");
+<?php
+require_once("config.php");
+require_once("PHPMailer/PHPMailer.php");
+require_once("PHPMailer/Exception.php");
+require_once("PHPMailer/SMTP.php");
 if(isset($_POST['subforgot'])){ 
-$login=$_REQUEST['login_var'];
-$query = "SELECT * from  users where (name='$login' OR email = '$login')"; 
-$res = mysqli_query($conn,$query);
-$count=mysqli_num_rows($res);
-//echo $count;
-if($count==1)
-{
-$findresult = mysqli_query($conn, "SELECT * FROM users WHERE (name='$login' OR email = '$login')");
-if($res = mysqli_fetch_array($findresult))
-{
-$oldftemail = $res['email'];  
-}
-$token = bin2hex(random_bytes(50));
- $inresult = mysqli_query($conn,"insert into pass_reset values('','$oldftemail','$token')"); 
- if ($inresult)  
- { 
-$FromName="Techno Smarter";
-$FromEmail="no_reply@technosmarter.com";
-$ReplyTo="technosmarterinfo@gmail.com";
-$credits="All rights are reserved | Techno Smarter "; 
-$headers  = "MIME-Version: 1.0\n";
-     $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-     $headers .= "From: ".$FromName." <".$FromEmail.">\n";
-      $headers .= "Reply-To: ".$ReplyTo."\n";
-      $headers .= "X-Sender: <".$FromEmail.">\n";
-       $headers .= "X-Mailer: PHP\n"; 
-       $headers .= "X-Priority: 1\n"; 
-       $headers .= "Return-Path: <".$FromEmail.">\n"; 
-         $subject="You have received password reset email"; 
-     $msg="Your password reset link <br> https://webshopwar.000webhostapp.com/Webstore-War/php/forgot_process.php?token=".$token." <br> Reset your password with this link .Click or open in new tab<br><br> <br> <br> <center>".$credits."</center>"; 
-   if(@mail($oldftemail, $subject, $msg, $headers,'-f'.$FromEmail) ){
-header("location:forgot-password.php?sent=1"); 
-$hide='1';
-          
+    $login = $_REQUEST['login_var'];
+    
+    $query = "SELECT * FROM users WHERE (name='$login' OR email='$login')"; 
+    $res = mysqli_query($conn, $query);
+    $count = mysqli_num_rows($res);
+
+    if($count == 1) {
+        $findresult = mysqli_query($conn, "SELECT * FROM users WHERE (name='$login' OR email='$login')");
+        if($res = mysqli_fetch_array($findresult)) {
+            $oldftemail = $res['email'];
+        }
+
+        $token = bin2hex(random_bytes(50));
+        $inresult = mysqli_query($conn, "INSERT INTO pass_reset VALUES ('', '$oldftemail', '$token')"); 
+
+        if ($inresult) {
+            // Initialize PHPMailer
+            $mail = new PHPMailer(true); // Passing `true` enables exceptions
+
+            try {
+                // Server settings
+                $mail->isSMTP(); // Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+                $mail->SMTPAuth = true; // Enable SMTP authentication
+                $mail->Username = 'your-email@yourdomain.com'; // SMTP username
+                $mail->Password = 'your-email-password'; // SMTP password
+                $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587; // TCP port to connect to
+
+                // Sender and recipient
+                $mail->setFrom('no_reply@technosmarter.com', 'Techno Smarter');
+                $mail->addAddress($oldftemail); // Add a recipient
+
+                // Content
+                $mail->isHTML(true); // Set email format to HTML
+                $mail->Subject = 'You have received password reset email';
+                $mail->Body = 'Your password reset link:<br>https://webshopwar.000webhostapp.com/Webstore-War/php/forgot_process.php?token=' . $token . '<br>Reset your password with this link. Click or open in new tab.<br><br>' . $credits;
+
+                // Send email
+                $mail->send();
+                
+                header("location:forgot-password.php?sent=1");
+                exit;
+            } catch (Exception $e) {
+                header("location:forgot-password.php?servererr=1");
+                exit;
+            }
+        } else {
+            header("location:forgot-password.php?something_wrong=1");
+            exit;
+        }
     } else {
-        
-    header("location:forgot-password.php?servererr=1"); 
-} 
-      } 
-      else 
-      { 
-          header("location:forgot-password.php?something_wrong=1"); 
-      }     
-}
-else  
-{
-header("location:forgot-password.php?err=".$login); 
-}
+        header("location:forgot-password.php?err=" . $login);
+        exit;
+    }
 }
 ?>
